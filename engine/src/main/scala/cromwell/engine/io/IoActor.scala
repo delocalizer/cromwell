c8f191d67a58add2808dfb536e08f1a69397fb30
Thibault Jeandet <tjeandet@broadinstitute.org>
2017-02-22 11:02:01 -0500

    wip

--- engine/src/main/scala/cromwell/engine/io/IoActor.scala
+++ engine/src/main/scala/cromwell/engine/io/IoActor.scala
@@ -9,14 +9,17 @@ import cromwell.core.io.IoCommand
 import cromwell.core.io.messages.IoRetry
 import cromwell.engine.io.IoActor._
 
+import scala.concurrent.ExecutionContext
 import scala.language.existentials
 
 final class IoActor(queueSize: Int, ioFlow: IoFlow)(implicit materializer: ActorMaterializer) extends Actor with ActorLogging with StreamActorHelper {
+  private val ec: ExecutionContext = context.dispatcher
+  
   private val source = Source.queue[IoCommandContext](queueSize, OverflowStrategy.dropNew)
   private val flow = ioFlow.flow
   private val sink = Sink.foreach[(Option[Any], IoCommandContext)] {
     case (Some(response: IoRetry[_]), commandContext) => 
-      context.system.scheduler.scheduleOnce(response.waitTime, self, commandContext.request.withNextBackoff)
+      context.system.scheduler.scheduleOnce(response.waitTime, self, commandContext.request.withNextBackoff)(ec, commandContext.replyTo)
       ()
     case (Some(response), commandContext) => commandContext.replyTo ! response
     case _ =>
